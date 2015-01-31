c8d802a219a1509b02347a7d798961a1a0ede471
Garrett Cox <garrettjcox@gmail.com>
2015-01-31 15:50:38 -0800

    Finally Implemented sortBy :D

--- src/underbar.js
+++ src/underbar.js
@@ -235,7 +235,25 @@
   // of that string. For example, _.sortBy(people, 'name') should sort
   // an array of people by their name.
   _.sortBy = function(collection, iterator) {
+    var tempArray = [];
+    if(iterator === 'length'){
+      collection.sort(function(a, b) {return a.length - b.length;})
+    }else if(_.containsNumber(collection)){
+      collection.sort(function(a, b) {return a - b;})
+    }else{
+      collection.sort(iterator)
+    }
+    return collection;
   };
+
+  // Returns true if collection contains a number.
+  _.containsNumber = function(collection){
+    var result = false;
+    _.each(collection, function(item){
+      if (typeof item === 'number') result = true;
+    });
+    return result;
+  }
 
   // Zip together two or more arrays with elements of the same index
   // going together.
