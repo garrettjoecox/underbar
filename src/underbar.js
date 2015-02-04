e89ce7e0f8689a5f4726792c723efd6454860d24
Garrett Cox <garrettjcox@gmail.com>
2015-02-03 16:24:19 -0800

    One line refactor :D

--- src/underbar.js
+++ src/underbar.js
@@ -179,21 +179,11 @@
 
   // Memorize an expensive function's results by storing them. You may assume
   // that the function takes only one argument and that it is a primitive.
-  // memoize could be renamed to oncePerUniqueArgumentList; memoize does the
-  // same thing as once, but based on many sets of unique arguments.
-  //
-  // _.memoize should return a function that, when called, will check if it has
-  // already computed the result for the given argument and return that value
-  // instead if possible.
-  //
-  // Still don't understand fully
   _.memoize = function(func) {
     var history = {}
     return function(){
       var args = Array.prototype.slice.call(arguments);
-      if (history[args]){
-        return history[args];
-      }else{
+      if (!history[args]){
         history[args] = func.apply(null, args)
       }
       return history[args];
